0ae7135c634cc8da472ee77d1b499908f7c1b9d7
amirullazmi0 <amirullazmi0@gmail.com>
2024-05-31 08:02:07 +0700

    update

--- app/admin-rsaa/maklumat-pelayanan/FormMaklumatPelayanan.tsx
+++ app/admin-rsaa/maklumat-pelayanan/FormMaklumatPelayanan.tsx
@@ -45,7 +45,7 @@ const FormMaklumatPelayanan = () => {
 
     useEffect(() => {
         getData()
-    }, [data])
+    }, [alertUpdate])
 
     return (
         <div className='grid gap-2 w-full'>
